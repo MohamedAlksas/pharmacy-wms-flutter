9dcfe53d302f236682108cd4a65f3715e0a1aba3
MohamedAlksas <adelm8267@gmail.com>
2026-05-22 20:31:00 +0300

    Point ApiConfig to live Somee.com backend

--- lib/Services/api_config.dart
+++ lib/Services/api_config.dart
@@ -1,5 +1,4 @@
 class ApiConfig {
   // Change this to your production URL when deploying:
-  // 'https://chemistore.runasp.net/api'
-  static const String baseUrl = 'http://localhost:5000/api';
+  static const String baseUrl = 'http://pharmacy-wms.somee.com/api';
 }
